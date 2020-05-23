94ebde0f2ef6f484d7ce9b2a46a6e7a02a13d67b
k2 <13083445730@163.com>
2020-05-23 15:58:32 +0800

    Update camshift_independent.py

--- camshift_independent.py
+++ camshift_independent.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class Trackobject(object):
     def __init__(self, scaling_factor=0.8):
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(1)
         _, self.frame = self.cap.read()
         self.scaling_factor = scaling_factor
         self.frame = cv2.resize(self.frame, None, fx=self.scaling_factor, fy=self.scaling_factor,
@@ -20,23 +20,25 @@ class Trackobject(object):
             _, frame = self.cap.read()
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             faces = np.array(self.face_cascade.detectMultiScale(gray, 1.3, 5))
+            # cv2.imshow('Face Tracker', frame)
             k = cv2.waitKey(30) & 0xff
             if k == 27:
                 cap.release()
                 cv2.destroyAllWindows()
                 sys.exit()
             if len(faces) != 0:
+                print 'face find'
                 faces_x = faces[:, 0]
                 faces_y = faces[:, 1]
                 faces_w = faces[:, 2]
                 faces_h = faces[:, 3]
                 for x, y, w, h in zip(faces_x, faces_y, faces_w, faces_h):
-                    self.selection = (x, y, x+w, y+h)
+                    self.selection = (x, y, x + w, y + h)
                     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2, 1)
-                    cv2.imshow('face_detect', frame)
+                    # cv2.imshow('face_detect', frame)
                     self.tracking_state = 1
-
-            print "box:", self.selection
+                break
+        print "box:", self.selection
 
     def start_tracking(self):
         while True:
@@ -44,9 +46,11 @@ class Trackobject(object):
             self.frame = cv2.resize(self.frame, None, fx=self.scaling_factor, fy=self.scaling_factor,
                                     interpolation=cv2.INTER_AREA)
             vis = self.frame.copy()
+            cv2.imshow('Object Tracker2', vis)
             hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
             mask = cv2.inRange(hsv, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
-            if self.selection:
+            if self.tracking_state == 1:
+                print "box2:", self.selection
                 x0, y0, x1, y1 = self.selection
                 self.track_window = (x0, y0, x1 - x0, y1 - y0)
                 roi_hsv = hsv[y0:y1, x0:x1]
@@ -57,14 +61,15 @@ class Trackobject(object):
                 vis_roi = vis[y0:y1, x0:x1]
                 cv2.bitwise_not(vis_roi, vis_roi)
                 vis[mask == 0] = 0
-            if self.tracking_state == 1:
+                self.tracking_state += 1
+            if self.tracking_state == 2:
                 self.selection = None
                 hsv_backproj = cv2.calcBackProject([hsv], [0], self.hist, [0, 180], 1)
                 hsv_backproj &= mask
                 term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
                 track_box, self.track_window = cv2.CamShift(hsv_backproj, self.track_window, term_crit)
                 cv2.ellipse(vis, track_box, (0, 255, 0), 2)
-                cv2.imshow('Object 1', hsv_backproj)
+                # cv2.imshow('Object 1', hsv_backproj)
             cv2.imshow('Object Tracker', vis)
             key = cv2.waitKey(5)
             if key == 27:
@@ -73,5 +78,6 @@ class Trackobject(object):
 
 
 if __name__ == '__main__':
-    Trackobject().face_detect()
-    Trackobject().start_tracking()
+    dp = Trackobject()
+    dp.face_detect()
+    dp.start_tracking()
